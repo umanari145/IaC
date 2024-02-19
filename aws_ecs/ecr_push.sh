d7b90d3fa9b07c59e87f61b5d4bd4bd36b75035e
umanari145 <umanari145@gmail.com>
2024-02-20 00:22:41 +0900

    リポジトリ作成build&pushスクリプト

---
+++ aws_ecs/ecr_push.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# ECRリポジトリ名の設定
+NGINX_REPO_NAME="nginx-repository"
+PHP_FPM_REPO_NAME="php-fpm-repository"
+
+# AWSアカウントIDとデフォルトリージョンの取得
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+AWS_DEFAULT_REGION=$(aws configure get region)
+
+# Dockerイメージのタグ名
+NGINX_IMAGE_TAG="latest"
+PHP_FPM_IMAGE_TAG="latest"
+
+# DockerイメージのビルドとECRへのプッシュ
+build_and_push_image() {
+    local repo_name=$1
+    local image_tag=$2
+    local docker_file=$3 # Dockerfile名
+
+    # ECRリポジトリURIの設定
+    local ecr_uri="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${repo_name}"
+
+    # Dockerイメージのビルド
+    docker build -t "${repo_name}:latest" "${docker_file}"
+
+    # ECRへのログイン
+    aws ecr get-login-password | docker login --username AWS --password-stdin "${ecr_uri}"
+
+    # Dockerイメージのタグ付け
+    docker tag "${repo_name}:${image_tag}" "${ecr_uri}:${image_tag}"
+
+    # ECRへのプッシュ
+    docker push "${ecr_uri}:${image_tag}"
+}
+
+# NGINX DockerイメージのビルドとECRへのプッシュ
+# 第3引数にはNginxのDockerfileがあるディレクトリへのパスを指定してください
+build_and_push_image "${NGINX_REPO_NAME}" "${NGINX_IMAGE_TAG}" "./nginx"
+
+# PHP-FPM DockerイメージのビルドとECRへのプッシュ
+# 第3引数にはPHP-FPMのDockerfileがあるディレクトリへのパスを指定してください
+build_and_push_image "${PHP_FPM_REPO_NAME}" "${PHP_FPM_IMAGE_TAG}" "./php-fpm"
---
+++ aws_ecs/php-fpm/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <title>test</title>
+  </head>
+  <body>
+    <h1>title</h1>
+    <?php echo '本日は晴天なり';?>
+  </body>
+</html>
